42c0b12a0e8c90cb8d92f3af27678be4055929d4
Emili <ejanchevis@gmail.com>
2023-10-03 17:12:23 -1000

    first commit

---
+++ geometry.py
@@ -0,0 +1,11 @@
+#Exercises
+# Calculate the area of a circle
+# radius = r
+def circle_area(r):
+    return round(pi * r**2, 2)
+
+
+# Calculate hypotenuse
+def hypotenuse(a, b):
+    return sqrt(a**2 + b**2)
+
