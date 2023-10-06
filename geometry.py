c1961e5d38487e4387180ac6b19eda817949d6b0
Emili <ejanchevis@gmail.com>
2023-10-06 09:50:38 -1000

    fixed exercise 1

--- geometry.py
+++ geometry.py
@@ -1,11 +1,14 @@
 #Exercises
 # Calculate the area of a circle
 # radius = r
+from math import pi
+
 def circle_area(r):
     return round(pi * r**2, 2)
 
 
 # Calculate hypotenuse
+from math import sqrt
+
 def hypotenuse(a, b):
     return sqrt(a**2 + b**2)
-
